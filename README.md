3294642723353d19024f3075de01516c2bfbebdf
发明控 <1258512088@QQ.com>
2022-11-30 15:44:30 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # fast_return_to_Beijing
 快速回京解除弹窗辅助器
+### 快速通知状态 最快4小时解除弹窗 
+* 首先在北京12345中反馈弹窗纠错
+* 提交完成后 打卡浏览器 进入 https://yqgz.beijing.gov.cn/wssryfw
+* 进入浏览器开发工具 （F12）
+* 填写身份证号 点击立即登陆 
+* 在请求中 按照图中选择 
+![](./1.png)
+* 复制到内容中 body 中的内容填入 gotu_beijing.py 第五行
+* 其中第33行需要iPhone用户安装bark 添加用户key 其他平台可替换其他推送方式 例如短信等 
+* pip3 install requests  （可能需要安装）
+* python3 gotu_beijing.py 
+# 等待通知即可 
+### 注意 在输出审核中时候 给社区打电话 即可加速审核 
---
+++ gotu_beijing.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import time
+
+bufbody = "将body内容复制到这里"
+
+def push():
+    global bufbody
+    post_url='https://yqgz.beijing.gov.cn/service/returnBj/checkPersonApplyStatus'
+    res=requests.post(post_url,data = bufbody ,headers={"accept": "application/json, text/plain, */*",
+        "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "content-type": "application/json",
+        "sec-ch-ua": "\"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"macOS\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin"})
+    print(res)
+    print(res.text)
+    return res.text
+
+if __name__ == '__main__':
+    checkState = 0
+    while True:
+        try:
+            res = json.loads(push())
+            State = q["data"]['checkState']
+            if State != checkState and checkState != 0 :
+                checkState = State
+                print("状态发生改变！！！！请尽快查看")
+                #这条可注释 需要bark app进行推送消息到手机
+                requests.get("https://api.day.app/填写你的bark app key /%E8%BF%9E%E6%8E%A5%E7%8A%B6%E6%80%81%E6%94%B9%E5%8F%98%E8%AF%B7%E7%82%B9%E5%87%BB%E9%93%BE%E6%8E%A5%E6%9F%A5%E7%9C%8B?url=https://yqgz.beijing.gov.cn/wssryfw/")
+        except:
+                print("请求故障")
+        time.sleep(60)
